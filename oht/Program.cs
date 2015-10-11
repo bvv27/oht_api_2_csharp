969f7a057640e37a1b9d234a2a00db8efc3056f8
bvv27 <bvv.developer@gmail.com>
2015-10-11 22:21:11 +0300

    0002

--- oht/Program.cs
+++ oht/Program.cs
@@ -13,7 +13,7 @@ namespace oht
         [STAThread]
         static void Main()
         {
-            //0001
+            //0002
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new FormExamples());
